d26e85041aae76577e4a3fcbd93fe8b28b5780e2
student <ballard2@madisoncollege.edu>
2024-03-09 20:28:18 -0600

    starting generic dao

---
+++ src/main/java/ClimbLog/persistence/GenericDao.java
@@ -0,0 +1,46 @@
+package ClimbLog.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
+/**
+ * generic DAO
+ */
+public class GenericDao<T> {
+    private Class<T> type;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+    /**
+     * Instantiates a new Generic dao
+     *
+     * @param type the entity type, for ex. User.
+     */
+    public GenericDao(Class<T> type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets an entity by id
+     * @param id entity id to search by
+     * @return a entity
+     */
+    public <T>T getById(int id) {
+        Session session = getSession();
+        T entity = (T)session.get(type, id);
+        session.close();
+        return entity;
+    }
+
+
+
+    /**
+     * Returns an open session from the SessionFactory
+     * @return session
+     */
+    private Session getSession() {
+        return SessionFactoryProvider.getSessionFactory().openSession();
+    }
+}
