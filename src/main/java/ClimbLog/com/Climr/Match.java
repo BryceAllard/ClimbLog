ddc56c5a464b55bbeb4ab8da5ec0a25065e340a3
student <ballard2@madisoncollege.edu>
2024-03-17 23:21:52 -0500

    added pom changes to work with API, got first api test working but on to second half of wk8act3 video

---
+++ src/main/java/ClimbLog/com/Climr/Match.java
@@ -0,0 +1,97 @@
+package ClimbLog.com.Climr;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Match{
+
+	@JsonProperty("fav_animal")
+	private String favAnimal;
+
+	@JsonProperty("rating")
+	private int rating;
+
+	@JsonProperty("phone_number")
+	private String phoneNumber;
+
+	@JsonProperty("id")
+	private int id;
+
+	@JsonProperty("type")
+	private String type;
+
+	@JsonProperty("email")
+	private String email;
+
+	@JsonProperty("username")
+	private String username;
+
+	public void setFavAnimal(String favAnimal){
+		this.favAnimal = favAnimal;
+	}
+
+	public String getFavAnimal(){
+		return favAnimal;
+	}
+
+	public void setRating(int rating){
+		this.rating = rating;
+	}
+
+	public int getRating(){
+		return rating;
+	}
+
+	public void setPhoneNumber(String phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPhoneNumber(){
+		return phoneNumber;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setType(String type){
+		this.type = type;
+	}
+
+	public String getType(){
+		return type;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public void setUsername(String username){
+		this.username = username;
+	}
+
+	public String getUsername(){
+		return username;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Match{" + 
+			"fav_animal = '" + favAnimal + '\'' + 
+			",rating = '" + rating + '\'' + 
+			",phone_number = '" + phoneNumber + '\'' + 
+			",id = '" + id + '\'' + 
+			",type = '" + type + '\'' + 
+			",email = '" + email + '\'' + 
+			",username = '" + username + '\'' + 
+			"}";
+		}
+}
---
+++ src/test/java/persistence/TestServiceClient.java
@@ -0,0 +1,28 @@
+package persistence;
+
+import ClimbLog.com.Climr.Match;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestServiceClient {
+
+    @Test
+    public void testswapiJSON() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target("https://my.api.mockaroo.com/climbr.json?key=d8027830&id=1");
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        //assertEquals("???", response);
+        ObjectMapper mapper = new ObjectMapper();
+        Match[] matches = mapper.readValue(response, Match[].class);
+        Match match = matches[0];
+        assertEquals(1, match.getId());
+    }
+}
