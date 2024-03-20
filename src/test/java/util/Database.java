304dd32f3561c93daf0c5a5dcc727739ed84b529
student <ballard2@madisoncollege.edu>
2024-03-20 16:19:05 -0500

    added screenshot of DB ERD and replaced sys.out.println with logger.error's

--- src/test/java/util/Database.java
+++ src/test/java/util/Database.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Provides access to the database
@@ -18,6 +20,8 @@ import java.util.Properties;
  */
 
 public class Database {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     // create an object of the class Database
     private static Database instance = new Database();
@@ -40,10 +44,10 @@ public class Database {
         try {
             properties.load (this.getClass().getResourceAsStream("/database.properties"));
         } catch (IOException ioe) {
-            System.out.println("Database.loadProperties()...Cannot load the properties file");
+            logger.error("Database.loadProperties()...Cannot load the properties file", ioe);
             ioe.printStackTrace();
         } catch (Exception e) {
-            System.out.println("Database.loadProperties()..." + e);
+            logger.error("Database.loadProperties()...", e);
             e.printStackTrace();
         }
 
@@ -86,7 +90,7 @@ public class Database {
             try {
                 connection.close();
             } catch (SQLException e) {
-                System.out.println("Cannot close connection" + e);
+                logger.error("Cannot close connection", e);
             }
         }
 
@@ -122,9 +126,9 @@ public class Database {
             }
 
         } catch (SQLException se) {
-            System.out.println("SQL Exception" + se);
+            logger.error("SQL Exception", se);
         } catch (Exception e) {
-            System.out.println("Exception" + e);
+            logger.error("Exception" + e);
         } finally {
             disconnect();
         }
